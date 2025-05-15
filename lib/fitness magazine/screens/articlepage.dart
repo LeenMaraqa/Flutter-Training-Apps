db54017aa8f8da80e222d81cf8af3fa15974a4bc
LeenMaraqa <leenmaraqa.1@gmail.com>
2025-05-15 10:52:30 +0300

    feat(fitness-magazine): build article detail page

---
+++ lib/fitness magazine/screens/articlepage.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_training_apps/fitness%20magazine/model/data.dart';
+import 'package:flutter_training_apps/fitness%20magazine/screens/homepage.dart';
+
+class SecondInterface extends StatelessWidget {
+  final Article article;
+  final String tagPrefix;
+
+  const SecondInterface({
+    super.key,
+    required this.article,
+    required this.tagPrefix,
+  });
+
+  List<Article> relatedArticles() {
+    List<Article> relatedList = [];
+
+    for (var a in Article.articles) {
+      if (a.category == article.category && a.id != article.id) {
+        relatedList.add(a);
+      }
+    }
+
+    return relatedList.take(2).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final relatedArt = relatedArticles();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Stack(
+              children: [
+                Hero(
+                  tag: '$tagPrefix-${article.id}',
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: CachedNetworkImage(
+                      imageUrl: article.image,
+                      fit: BoxFit.cover,
+                      placeholder:
+                          (context, url) => Center(
+                            child: Image.asset(
+                              'assets/assets/images/png/placeholder.png',
+                            ),
+                          ),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 40,
+                  right: 16,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                Positioned(
+                  top: 285,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Text(
+                        article.title,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontFamily: 'Somar',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 30),
+                      child: Text(
+                        article.body,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontFamily: 'Somar',
+                          fontWeight: FontWeight.w200,
+                        ),
+                      ),
+                    ),
+                    if (relatedArticles().isNotEmpty)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20),
+                          Divider(thickness: 1, color: Colors.grey[300]),
+                          SizedBox(height: 20),
+                          Text(
+                            'مقالات ذات صلة ',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontFamily: 'Somar',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          ListView.builder(
+                            padding: EdgeInsets.symmetric(vertical: 35),
+                            shrinkWrap: true,
+                            itemCount: relatedArt.length,
+                            itemBuilder: (context, index) {
+                              final article = relatedArt[index];
+                              return CategoryCard(
+                                article: article,
+                                tagPrefix: 'category',
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
